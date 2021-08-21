e1da93f3bfe97783759d68b30c29075447b6a3db
YuYoungGyu <54985877+YuYoungGyu@users.noreply.github.com>
2021-08-21 20:58:48 +0900

    edit and add data code
    edit and add data code

--- code_for_article_data/pos_to_dict.py
+++ code_for_article_data/pos_to_dict.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import glob
 
-def pos_to_dict(df):
+def weekly_pos_to_dict(df):
     df = df.groupby(['week']).sum()
     weekly_keyword = df.iloc[:,3]
 
@@ -18,3 +18,27 @@ def pos_to_dict(df):
     weekly_df_all = pd.DataFrame(weekly_dic_all)
     weekly_df_all = weekly_df_all.fillna(0)
     return weekly_df_all
+
+def monthly_pos_to_dict(df):
+    week_col = df['week']
+
+    month_col = []
+    for w in week_col:
+        month_col.append(w//4)
+    df['month'] = month_col
+    keyword_month = df
+    monthly_pos = keyword_month.groupby('month')['pos'].sum()
+
+    monthly_word_dict = {}
+
+    for m in range(len(monthly_pos)):
+        word_dict = {}
+        for word in monthly_pos[m]:
+            if word not in word_dict:
+                word_dict[word] = 1
+            else:
+                word_dict[word] += 1
+        monthly_word_dict[m] = word_dict
+    monthly_word_df = pd.DataFrame(monthly_word_dict)
+    monthly_word_df = monthly_word_df.fillna(0)
+    return monthly_word_df
